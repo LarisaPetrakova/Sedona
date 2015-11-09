7d3e9086ed4c1e20d3c51502e55820aefbe49cd5
Larisa Petrakova <larisa.petrakova@gmail.com>
2015-11-09 10:15:38 +0300

    Добавлен javascript, сделанный полностью

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,29 @@
 var link = document.querySelector(".hotel-search");
 var popup = document.querySelector(".hotel-form");
+var datein = popup.querySelector("[name='date-in']");
+var dateout = popup.querySelector("[name='date-out']");
+var storage = localStorage.getItem("datein");
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.toggle("hotel-form-show");
+    if (storage) {
+          datein.value = storage;
+          dateout.focus();
+        } else {
+          datein.focus();
+        }
 });
 
 popup.addEventListener("submit", function (event) {
-        event.preventDefault();
+     if (!datein.value || !dateout.value) {
+         event.preventDefault();
+         popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+        } else {
+          localStorage.setItem("datein", datein.value);
+        }
 });
 window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
@@ -15,3 +31,4 @@ window.addEventListener("keydown", function (event) {
         popup.classList.remove("hotel-form-show");
     }
 });
+
